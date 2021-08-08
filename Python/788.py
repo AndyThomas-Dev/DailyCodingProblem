5b7b8ccaa6b154680462290d61efacc5f9877a8e
AndyThomas-Dev <57837331+AndyThomas-Dev@users.noreply.github.com>
2021-08-08 09:24:50 +0100

    Update 788.py

--- Python/788.py
+++ Python/788.py
@@ -1,4 +1,10 @@
+# Problem #788
 
+# Write a program that checks whether an integer is a palindrome. 
+
+# For example, 121 is a palindrome, as well as 888. 678 is not a palindrome. 
+
+# Do not convert the integer into a string.
 
 
 def getDigits(num):
