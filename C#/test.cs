13bbf656400e5aae98d795132dc3bdebf1cad747
AndyThomas-Dev <57837331+AndyThomas-Dev@users.noreply.github.com>
2022-04-14 21:34:02 +0100

    Problem #1024 - Reverse bits C#
    Given a 32-bit integer, return the number with its bits reversed.

---
+++ C#/test.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+public class Hello
+{
+  static void Main(string[] args)
+  {
+
+    string output = "";
+    
+    foreach (char c in args[0]){
+      output += Convert.ToString(c, 2).PadLeft(8, '0');
+    }
+
+    Console.WriteLine(output);
+
+    string reversed = "";
+
+    foreach (char c in output){
+      char r = c == '1' ? '0' : '1';
+      reversed += Convert.ToString(r);
+    }
+      
+    Console.WriteLine(reversed);
+
+  }
+}
