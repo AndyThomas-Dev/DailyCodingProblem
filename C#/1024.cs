058f5e01c0f399976c6fc3aab473a2e7992c24d3
AndyThomas-Dev <57837331+AndyThomas-Dev@users.noreply.github.com>
2022-04-14 22:03:19 +0100

    Refactor
    Chunk out to helper functions

--- C#/1024.cs
+++ C#/1024.cs
@@ -4,28 +4,40 @@ using System.Linq;
 
 public class ReverseBits
 {
-  static void Main(string[] args)
-  {
     
     // Given a 32-bit integer, return the number with its bits reversed.
     // Note: I took this to mean the character encoding of the binary number
 
-    string output = "";
-    
-    foreach (char c in args[0]){
-      output += Convert.ToString(c, 2).PadLeft(8, '0');
+    static void Main(string[] args)
+    {   
+      Console.WriteLine(FlipBits(ConvertToBinary(args[0])));
     }
 
-    // Console.WriteLine(output);
+    private static string ConvertToBinary(string input)
+    {
 
-    string reversed = "";
+      string output = "";
 
-    foreach (char c in output){
-      char r = c == '1' ? '0' : '1';
-      reversed += Convert.ToString(r);
+      foreach (char c in input)
+      {
+        output += Convert.ToString(c, 2).PadLeft(8, '0');
+      }
+
+      // Console.WriteLine(output);
+      return output;
     }
-      
-    Console.WriteLine(reversed);
 
-  }
+    private static string FlipBits(string input)
+    {
+
+      string reversed = "";
+
+      foreach (char c in input)
+      {
+        char r = c == '1' ? '0' : '1';
+        reversed += Convert.ToString(r);
+      }
+
+      return reversed;
+    }
 }
