52ac1fac323f1cadac601337ed9616f68ff46c0d
AndyThomas-Dev <57837331+AndyThomas-Dev@users.noreply.github.com>
2023-03-11 14:21:56 +0000

    Update 1318.cs

--- C#/1318.cs
+++ C#/1318.cs
@@ -4,7 +4,7 @@ namespace MainProj;
 
 class Program
 {
-    // Daily Coding Problem: Problem #1318
+    // Daily Coding Problem 1318
     // You are given a string of length N and a parameter k.The string can be manipulated by taking one of the first k letters and moving it to the end.
 
     // Write a program to determine the lexicographically smallest string that can be created after an unlimited number of moves.
@@ -23,10 +23,13 @@ class Program
             Assert.That(result == "ailyyzd");
 
             result = MutateString("daily", 2);
-            Assert.That(result == "ilyda");
+            Assert.That(result == "ilyad");
 
             result = MutateString("zzzzzzA", 6);
             Assert.That(result == "Azzzzzz");
+
+            result = MutateString("zzazzzA", 6);
+            Assert.That(result == "Aazzzzz");
         }
         catch(Exception ex)
         {
@@ -38,6 +41,11 @@ class Program
 
     private static string MutateString(string input, int k)
     {
-        return input.Substring(k) + input.Substring(0, k);
+        var before = input[..k];
+
+        // Sort alphabetically
+        var sortedBefore = string.Concat(before.OrderBy(c => c));
+
+        return input.Substring(k) + sortedBefore;
     }
 }
